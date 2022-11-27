4fa2591bc1c85dffad6f6ab95ca4a6710163e2f5
Sangnn <sangnnph14292@fpt.edu.vn>
2022-11-27 17:12:06 +0700

    code chức năng thống kê

---
+++ app/src/main/java/com/example/doctruyen_iread/Adapter/AdapterThongKe.java
@@ -0,0 +1,77 @@
+package com.example.doctruyen_iread.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.doctruyen_iread.FragmentTrangChu.ReadChapterActivity;
+import com.example.doctruyen_iread.Module.Story;
+import com.example.doctruyen_iread.R;
+
+import java.util.ArrayList;
+
+public class AdapterThongKe extends RecyclerView.Adapter<AdapterThongKe.Holder> {
+    Context mContext;
+    ArrayList<Story> stories = new ArrayList<>();
+
+    public AdapterThongKe(Context mContext) {
+        this.mContext = mContext;
+    }
+
+
+    public void getData (ArrayList<Story> stories) {
+        this.stories = stories;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_thong_ke, parent, false);
+        return new Holder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        holder.image.setImageResource(R.drawable.anh3);
+        holder.title.setText(stories.get(position).getStoryTitle());
+        holder.readTime.setText("Lượt Xem: " + stories.get(position).getStoryViews());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, ReadChapterActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("chapter", stories.get(holder.getAdapterPosition()).getStoryTitle());
+                intent.putExtra("story", bundle);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return stories.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        private final ImageView image;
+        private final TextView title;
+        private final TextView readTime;
+        private final CardView cardView;
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.imvStoryThongKe);
+            title = itemView.findViewById(R.id.tvTitleThongKe);
+            readTime = itemView.findViewById(R.id.tvReadTime);
+            cardView = itemView.findViewById(R.id.cardView);
+        }
+    }
+}
--- app/src/main/java/com/example/doctruyen_iread/FragmentThem/ThongKe.java
+++ app/src/main/java/com/example/doctruyen_iread/FragmentThem/ThongKe.java
@@ -1,24 +1,77 @@
 package com.example.doctruyen_iread.FragmentThem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.doctruyen_iread.Adapter.AdapterThongKe;
 import com.example.doctruyen_iread.MainActivity;
+import com.example.doctruyen_iread.Module.Story;
 import  com.example.doctruyen_iread.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toolbar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ThongKe extends AppCompatActivity {
-
-    Toolbar toolbar;
+    Toolbar toolbar ;
+    private RecyclerView recyclerView;
+    private final ArrayList<Story> stories = new ArrayList<>();
+    private final AdapterThongKe adapter = new AdapterThongKe(this);
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final CollectionReference colRef = db.collection("Story");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_ke);
 
-        toolbar = findViewById(R.id.toobar_ve2);
+        toolbar = findViewById(R.id.toobar_ve);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = findViewById(R.id.rvThongKe);
+
+        updateLVStories();
     }
+    private void updateLVStories() {
+        if (stories.size() > 0)
+            stories.clear();
+
+        colRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    DocumentSnapshot doc = document;
+                    Story story = doc.toObject(Story.class);
+                    stories.add(story);
+                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ThongKe.this, LinearLayoutManager.VERTICAL, false);
+                    recyclerView.setLayoutManager(linearLayoutManager);
+
+                    adapter.getData(stories);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ThongKe.this, "Lỗi", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
